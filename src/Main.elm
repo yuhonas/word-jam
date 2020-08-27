7841d354d05229c48cbdc0bab7f74bf259d0ccce
Clint <clint@yuhonas.com>
2020-08-27 17:42:21 +1000

    testing style purging on prod

--- src/Main.elm
+++ src/Main.elm
@@ -22,9 +22,9 @@ update msg model =
 view model =
   div []
     [ button [ class """
-    bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded
+    bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded
     """, onClick
     Decrement ] [ text "Decrement" ]
-    , div [] [ text (String.fromInt model) ]
-    , button [ class "bg-blue-500", onClick Increment ] [ text "Increment" ]
+    , div [ class "text-6xl" ] [ text (String.fromInt model) ]
+    , button [ class "bg-purple-500 text-4xl text-white", onClick Increment ] [ text "Increment" ]
     ]
